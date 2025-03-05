26cc1d20c56126d5382bcabf2e895e9a4239ea82
Mohit Paudel <paudel823@gmail.com>
2025-03-05 23:31:29 +0545

    Added post adding method

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -20,7 +22,29 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Ensure user is authenticated
+        if (!$request->user()) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'category_id' => 'required|exists:categories,id',
+            // 'author_id' => 'required|int',
+            'status' => 'required|in:published,draft'
+        ]);
+
+        Post::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'category_id' => $request->category_id,
+            'author_id' => $request->user()->id,
+            'status' => $request->status
+        ]);
+
+        return response()->json(['message' => 'Post created successfully']);
     }
 
     /**
