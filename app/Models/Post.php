4d2d78617ebb22006c170ec549705823da6d40a3
Mohit Paudel <paudel823@gmail.com>
2025-03-05 20:42:09 +0545

    Added relationship to the model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $guarded = [];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }
