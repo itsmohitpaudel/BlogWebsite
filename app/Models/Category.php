f1dcef034e96cab92755e8dc7f022190582fc1f7
Mohit Paudel <paudel823@gmail.com>
2025-03-05 21:22:56 +0545

    Added slug setter for category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,10 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
     protected $guarded = [];
+
+    public function setCategoryNameAttribute($value)
+    {
+        $this->attributes['category_name'] = $value;
+        $this->attributes['category_slug'] = Str::slug($value);
+    }
 
     public function posts()
     {
