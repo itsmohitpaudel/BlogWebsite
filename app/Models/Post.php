efd91eb5f8833475bcb58b04d6d2fc20bca3d8de
Mohit Paudel <paudel823@gmail.com>
2025-03-05 22:30:43 +0545

    Add setter method for slug

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,10 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     protected $guarded = [];
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 
     public function category()
     {
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,10 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Tag extends Model
 {
     protected $guarded = [];
+
+    public function setTagNameAttribute($value)
+    {
+        $this->attributes['tag_name'] = $value;
+        $this->attributes['tag_slug'] = Str::slug($value);
+    }
 
     public function posts()
     {
