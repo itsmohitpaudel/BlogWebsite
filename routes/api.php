6c2cedae06464fab06a42f8fee913bcd8aecb047
Mohit Paudel <paudel823@gmail.com>
2025-03-05 20:34:44 +0545

    Added api route to api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TagController;
+use App\Http\Controllers\Api\CommentController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+Route::apiResource('users', UserController::class);
+Route::apiResource('posts', PostController::class);
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('tags', TagController::class);
+Route::apiResource('comments', CommentController::class);
