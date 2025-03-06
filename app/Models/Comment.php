b7fb7b751c0454b8c437e485740eaef205064cd6
Mohit Paudel <paudel823@gmail.com>
2025-03-06 22:15:21 +0545

    Defining the polimorphic relationship on comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,13 +8,13 @@ class Comment extends Model
 {
     protected $guarded = [];
 
-    public function post()
-    {
-        return $this->belongsTo(Post::class);
-    }
-
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function commentable()
+    {
+        return $this->morphTo();
     }
 }
