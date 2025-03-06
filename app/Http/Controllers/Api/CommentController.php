45ba5cf10175b9d6d9c79677cc5bd4693d6e6b78
Mohit Paudel <paudel823@gmail.com>
2025-03-06 22:51:27 +0545

    Created comment policy and updated comment controller

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -3,24 +3,42 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class CommentController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Post $post)
     {
-        //
+        return response()->json([
+            'message' => 'Comments retrieved successfully',
+            'data' => $post->comments()->with('user')->get()
+        ], 200);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Post $post)
     {
-        //
+        $validatedData = $request->validate([
+            'content' => 'required|string'
+        ]);
+
+        $comment = new Comment($validatedData);
+        $comment->user_id = Auth::id();
+        $post->comments()->save($comment); //Here, commentable id and commentable_type are auto assigned
+
+        return response()->json([
+            'message' => 'Comment added successfully',
+            'data' => $comment
+        ], 201);
     }
 
     /**
@@ -42,8 +60,29 @@ class CommentController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $comment = Comment::where('id', $id)->first();
+
+        if (!$comment) {
+            return response()->json([
+                'message' => 'Comment not found',
+                'data' => null
+            ], 404);
+        }
+
+        if (!Gate::allows('delete', $comment)) {
+            return response()->json([
+                'message' => 'Unauthorized',
+                'data' => null
+            ], 403);
+        }
+
+        $comment->delete();
+
+        return response()->json([
+            'message' => 'Comment deleted successfully',
+            'data' => null
+        ], 200);
     }
 }
