099c7fd85f3f6e642c4fc3d2609ef6d5b0705c69
Mohit Paudel <paudel823@gmail.com>
2025-03-06 21:21:45 +0545

    Adding functionality with authorization to UserController

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -12,7 +15,19 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        if (!Gate::allows('viewAny', User::class)) {
+            return response()->json([
+                'message' => 'Unauthorized',
+                'data' => null,
+            ], 403);
+        }
+
+        $users = User::latest()->get();
+
+        return response()->json([
+            'message' => 'Users retrieved successfully',
+            'data' => $users
+        ], 200);
     }
 
     /**
@@ -20,15 +35,53 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if (!Gate::allows('create', User::class)) {
+            return response()->json([
+                'message' => 'Unauthorized',
+                'data' => null,
+            ], 403);
+        }
+
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+            'role' => 'required|in:admin,author' //Admin assigns roles
+        ]);
+
+        // Hashing the entered passowrd
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        $user = User::create($validatedData);
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'data' => $user
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $user = User::where('id', $id)->first();
+
+        // Checks if the user exists
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+                'data' => null
+            ], 404);
+        }
+
+        // Checks authorization
+        Gate::authorize('view', $user);
+
+        return response()->json([
+            'message' => 'User retrieved successfully',
+            'data' => $user
+        ], 200);
     }
 
     /**
