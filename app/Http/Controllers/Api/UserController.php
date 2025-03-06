1062efca3366a2ebe4150b9ae30fec7605c0b27d
Mohit Paudel <paudel823@gmail.com>
2025-03-06 21:48:27 +0545

    Changing variables name and updates

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Hash;
 
@@ -65,10 +66,10 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        $user = User::where('id', $id)->first();
+        $person = User::where('id', $id)->first();
 
         // Checks if the user exists
-        if (!$user) {
+        if (!$person) {
             return response()->json([
                 'message' => 'User not found',
                 'data' => null
@@ -76,27 +77,129 @@ class UserController extends Controller
         }
 
         // Checks authorization
-        Gate::authorize('view', $user);
+        Gate::authorize('view', $person);
 
         return response()->json([
             'message' => 'User retrieved successfully',
-            'data' => $user
+            'data' => $person
         ], 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $user = User::where('id', $id)->first();
+
+        // If the user is not found
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+                'data' => null
+            ], 404);
+        }
+
+        if (!Gate::allows('update', $user)) {
+            return response()->json([
+                'message' => 'Unauthorized',
+                'data' => null,
+            ], 403);
+        }
+
+        $validatedData = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'email' => 'sometimes|email|unique:users,email,' . $user->id,
+            'password' => 'sometimes|min:6'
+        ]);
+
+        if (isset($validatedData['password'])) {
+            $validatedData['password'] = Hash::make($validatedData['password']);
+        }
+
+        $user->update($validatedData);
+
+        return response()->json([
+            'message' => 'User updated successfully',
+            'data' => $user
+        ], 200);
+    }
+
+    public function updateRole(Request $request, $id)
+    {
+        // Finds the user
+        $person = User::where('id', $id)->first();
+
+        $loggedInUser = Auth::id();
+
+        // return response()->json([
+        //     'messsage' => 'Hi',
+        //     'data' => $person
+        // ]);
+
+        if (!$person) {
+            return response()->json([
+                'message' => 'User not found',
+                'data' => null
+            ], 404);
+        }
+
+        if (!Gate::allows('updateRole', $person)) {
+            return response()->json([
+                'message' => 'Unauthorized',
+                'data' => null
+            ], 403);
+        }
+
+        $validatedData = $request->validate([
+            'role' => 'required|in:admin,author'
+            //Only valid roles allowed
+        ]);
+
+        // To prevent self-role change to avoid loosing admin role
+        if (Auth::id() === $person->id) {
+            return response()->json([
+                'message' => 'You cannot change your own role',
+                'data' => null
+            ], 403);
+        }
+
+        // Updating the role
+        $person->update(['role' => $validatedData['role']]);
+
+        return response()->json([
+            'message' => 'User role updated successfully',
+            'data' => $person
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        // Finds the user
+        $person = User::where('id', $id)->first();
+
+        if (!$person) {
+            return response()->json([
+                'message' => 'User not found',
+                'data' => null
+            ], 404);
+        }
+
+        if (!Gate::allows('delete', $person)) {
+            return response()->json([
+                'message' => 'Unauthorized',
+                'data' => null
+            ], 403);
+        }
+
+        $person->delete();
+
+        return response()->json([
+            'message' => 'User deleted successfully',
+            'data' => null
+        ], 204);
     }
 }
