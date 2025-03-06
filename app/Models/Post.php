5a715fa5a70bda6ef87eeeb8f9c23a0e75757823
Mohit Paudel <paudel823@gmail.com>
2025-03-06 22:17:46 +0545

    Added relationship with comments and tags

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -24,4 +24,14 @@ class Post extends Model
     {
         return $this->belongsTo(User::class, 'author_id');
     }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
 }
